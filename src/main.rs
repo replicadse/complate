c419dbcdf56faf870e50600474e87c5b1de4ccba
Heiko Alexander Weber <heiko.a.weber@gmail.com>
2020-01-27 22:38:17 +0100

    + config example

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
+extern crate clap;
+use clap::{App, Arg};
+
 fn main() -> std::io::Result<()> {
     env_logger::init();
-    println!("main");
+    App::new("complate")
+       .version("0.1.0")
+       .about("A git commit buddy.")
+       .author("Weber, Heiko Alexander <heiko.a.weber@gmail.com>")
+       .arg(Arg::with_name("sample")
+            .short("s")
+            .long("sample")
+            .value_name("SAMPLE")
+            .help("Just a config example.")
+            .takes_value(true))
+       .get_matches();
     Ok(())
 }
