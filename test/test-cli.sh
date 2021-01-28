29a92b13e74829ca29620238641619de41e87c61
Heiko Alexander Weber <haw@voidpointergroup.com>
2021-01-28 15:15:23 +0000

    #1 cli tests

---
+++ test/test-cli.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+ev() {
+    if [ $2 -eq $1 ]
+    then
+        printf ""
+    else
+        printf "command failed\n" && exit 1
+    fi
+}
+
+echo "Testing with no shell trust"
+cmd="../target/debug/complate render -c=./config.yml -t=test"
+$cmd
+ev 1 $?
+
+echo "Testing with ultimate shell-trust"
+cmd="../target/debug/complate render -c=./config.yml -t=test --shell-trust=ultimate"
+$cmd
+ev 0 $?
+echo ""
+
+echo ""
+echo "All tests succeeded."
+echo ""
+exit 0
