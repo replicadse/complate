8b679bc9fb826b34eeaf8cceafc0bee2ca5f4f45
Alexander Weber <alex.weber@nexteraanalytics.com>
2023-03-02 14:40:28 +0100

    + HELM values example

---
+++ examples/helm/render.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# MY_SECRET_PASS should be set as env var in CI
+echo ">>> values dev <<<"
+echo ""
+complate -e render -t chart -v "version=0.1.0"
+echo ">>> values dev <<<"
+echo ""
+MY_SECRET_PASS="pass:superpass" complate -e render -t dev --helpers --shell-trust=ultimate
+echo ""
+echo ">>> values prod <<<"
+echo ""
+MY_SECRET_PASS="pass:superpass" complate -e render -t prod --helpers --shell-trust=ultimate
