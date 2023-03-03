fed2c4953436b815823a359eb997856c708af36d
replicadse <aw@voidpointergroup.com>
2023-03-04 00:27:55 +0100

    upgrade clap

--- src/args/mod.rs
+++ src/args/mod.rs
@@ -1,5 +1,8 @@
-use std::result::Result;
+use std::{result::Result, collections::HashMap};
 
+use clap::{Arg, ArgAction};
+
+#[derive(Debug)]
 pub struct CallArgs {
     pub privileges: Privilege,
     pub command: Command,
@@ -52,16 +55,19 @@ impl CallArgs {
     }
 }
 
+#[derive(Debug)]
 pub enum Privilege {
     Normal,
     Experimental,
 }
 
+#[derive(Debug)]
 pub enum Command {
     Init,
     Render(RenderArguments),
 }
 
+#[derive(Debug)]
 pub struct RenderArguments {
     pub configuration: String,
     pub template: Option<String>,
@@ -78,6 +84,7 @@ pub enum ShellTrust {
     Ultimate,
 }
 
+#[derive(Debug)]
 pub enum Backend {
     #[cfg(feature = "backend+cli")]
     CLI,
@@ -97,170 +104,108 @@ impl ClapArgumentLoader {
             backend_values.push("ui");
         }
 
-        let command = clap::App::new("complate")
+        let root_command = clap::Command::new("complate")
             .version(env!("CARGO_PKG_VERSION"))
             .about("A rusty text templating application for CLIs.")
             .author("replicadse <aw@voidpointergroup.com>")
-            .arg(clap::Arg::with_name("experimental")
-                    .short("e")
+            .propagate_version(true)
+            .subcommand_required(true)
+            .args([
+                Arg::new("experimental")
+                    .short('e')
                     .long("experimental")
-                    .value_name("EXPERIMENTAL")
-                    .help("Enables experimental features that do not count as stable.")
-                    .required(false)
-                    .takes_value(false))
-            .subcommand(clap::App::new("init")
+                    .help("enables experimental features")
+                    .num_args(0)
+            ])
+            .subcommand(clap::Command::new("init")
                 .about("Initializes a dummy default configuration in \"./.complate/config.yaml\"."))
-            .subcommand(clap::App::new("render")
+            .subcommand(clap::Command::new("render")
                 .about("Renders a template by replacing values as specified by the configuration.")
-                .arg(clap::Arg::with_name("config")
-                    .short("c")
+                .arg(clap::Arg::new("config")
+                    .short('c')
                     .long("config")
-                    .value_name("FILE")
                     .help("The configuration file to use.")
-                    .default_value("./.complate/config.yaml")
-                    .multiple(false)
-                    .required(false)
-                    .takes_value(true))
-                .arg(clap::Arg::with_name("template")
-                    .short("t")
+                    .default_value("./.complate/config.yaml"))
+                .arg(clap::Arg::new("template")
+                    .short('t')
                     .long("template")
-                    .value_name("TEMPLATE")
-                    .help("Specify the template to use from the config and skip it's selection.")
-                    .multiple(false)
-                    .required(false)
-                    .takes_value(true))
-                .arg(clap::Arg::with_name("shell-trust")
-                    .long("shell-trust")
-                    .value_name("SHELL_TRUST")
-                    .help("Enables the shell mode. This is potentially insecure and should only be done for trustworthy sources.")
-                    .possible_values(&["none", "ultimate"])
-                    .multiple(false)
-                    .required(false)
-                    .default_value("none")
-                    .takes_value(true))
-                .arg(clap::Arg::with_name("strict")
-                    .short("s")
+                    .help("Specify the template to use from the config and skip it's selection."))
+                .arg(clap::Arg::new("trust")
+                    .long("trust")
+                    .help("Enables the shell command execution. This is potentially insecure and should only be done for trustworthy sources.")
+                    .action(ArgAction::SetTrue))
+                .arg(clap::Arg::new("strict")
+                    .short('s')
                     .long("strict")
-                    .value_name("STRICT")
-                    .help("Defines whether the templating is done in strict mode (fail on missing value for variable).")
-                    .possible_values(&["true", "false"])
-                    .default_value("true")
-                    .multiple(false)
-                    .required(false)
-                    .takes_value(true))
-                .arg(clap::Arg::with_name("helpers")
+                    .action(ArgAction::SetTrue)
+                    .help("Defines whether the templating is done in strict mode (fail on missing value for variable)."))
+                .arg(clap::Arg::new("helpers")
                     .long("helpers")
-                    .value_name("HELPERS")
-                    .help("Enables handlebar helpers.")
-                    .multiple(false)
-                    .required(false)
-                    .takes_value(false))
-                .arg(clap::Arg::with_name("backend")
-                    .short("b")
+                    .action(ArgAction::SetTrue)
+                    .help("Enables handlebar helpers."))
+                .arg(clap::Arg::new("backend")
+                    .short('b')
                     .long("backend")
-                    .value_name("BACKEND")
                     .help("The execution backend (cli=native-terminal, ui=ui emulator in terminal).")
-                    .possible_values(backend_values.as_slice())
-                    .default_value(backend_values.first().unwrap())
-                    .multiple(false)
-                    .required(false)
-                    .takes_value(true))
-                .arg(clap::Arg::with_name("value")
-                    .short("v")
+                    .value_parser(backend_values.clone())
+                    .default_value(backend_values.first().unwrap()))
+                .arg(clap::Arg::new("value")
+                    .short('v')
                     .long("value")
-                    .value_name("VALUE")
-                    .help("Overrides a certain value definition with a string.")
-                    .multiple(true)
-                    .required(false)
-                    .takes_value(true)))
-            .get_matches();
+                    .help("Overrides a certain value definition with a string.")));
+        let command_matches = root_command.get_matches();
 
-        let privileges = if command.is_present("experimental") {
+        let privileges = if command_matches.get_flag("experimental") {
             Privilege::Experimental
         } else {
             Privilege::Normal
         };
 
-        if command.subcommand_matches("init").is_some() {
-            return Ok(CallArgs {
-                privileges,
+        if let Some(..) = command_matches.subcommand_matches("init") {
+            Ok(CallArgs {
                 command: Command::Init,
-            });
-        }
+                privileges
+            })
+        } else if let Some(subc) = command_matches.subcommand_matches("render") {
+            let config = std::fs::read_to_string(subc.get_one::<String>("config").unwrap())?;
+            let template = subc.get_one::<String>("template").map(|v| v.to_owned());
+            let shell_trust = if subc.get_flag("trust") {
+                ShellTrust::Ultimate
+            } else {
+                ShellTrust::None
+            };
+            let strict = subc.get_flag("strict");
 
-        match command.subcommand_matches("render") {
-            Some(x) => {
-                let config = if x.is_present("config") {
-                    let config_param = x.value_of("config").unwrap();
-                    std::fs::read_to_string(config_param.to_owned())?
-                } else {
-                    return Err(Box::new(crate::error::Failed::new(
-                        "configuration unspecified",
-                    )));
-                };
-
-                let template = if x.is_present("template") {
-                    Some(x.value_of("template").unwrap().to_owned())
-                } else {
-                    None
-                };
-
-                let shell_trust = match x.value_of("shell-trust") {
-                    Some(x) => match x {
-                        "none" => ShellTrust::None,
-                        "ultimate" => ShellTrust::Ultimate,
-                        _ => ShellTrust::None,
-                    },
-                    None => ShellTrust::None,
-                };
-
-                let strict = match x.value_of("strict") {
-                    Some(x) => x.parse::<bool>()?,
-                    None => true,
-                };
-
-                let mut value_overrides: std::collections::HashMap<String, String> =
-                    std::collections::HashMap::new();
-                if let Some(values_overrides_arg) = x.values_of("value") {
-                    for vo in values_overrides_arg {
-                        let spl: Vec<&str> = vo.splitn(2, "=").collect();
-                        value_overrides.insert(spl[0].to_owned(), spl[1].to_owned());
-                    }
+            let mut value_overrides = HashMap::<String, String>::new();
+            if let Some(vo_arg) = subc.get_many::<String>("value") {
+                for vo in vo_arg {
+                    let spl = vo.splitn(2, "=").collect::<Vec<_>>();
+                    value_overrides.insert(spl[0].to_owned(), spl[1].to_owned());
                 }
-
-                let backend = match x.value_of("backend") {
-                    Some(x) => match x {
-                        #[cfg(feature = "backend+cli")]
-                        "cli" => Backend::CLI,
-                        #[cfg(feature = "backend+ui")]
-                        "ui" => Backend::UI,
-                        _ => {
-                            return Err(Box::new(crate::error::Failed::new("no backend specified")))
-                        }
-                    },
-                    #[cfg(feature = "backend+cli")]
-                    None => Backend::CLI,
-                    #[cfg(feature = "backend+ui")]
-                    #[allow(unreachable_patterns)]
-                    None => Backend::UI,
-                };
-
-                let helpers = x.is_present("helpers");
-
-                Ok(CallArgs {
-                    privileges,
-                    command: Command::Render(RenderArguments {
-                        configuration: config,
-                        template,
-                        value_overrides,
-                        shell_trust,
-                        strict,
-                        backend,
-                        helpers,
-                    }),
-                })
             }
-            None => Err(Box::new(crate::error::UnknownCommand::default())),
+            let backend = match subc.get_one::<String>("backend").unwrap().as_str() {
+                #[cfg(feature = "backend+cli")]
+                "cli" => Backend::CLI,
+                #[cfg(feature = "backend+ui")]
+                "ui" => Backend::UI,
+                _ => return Err(Box::new(crate::error::Failed::new("no backend specified"))),
+            };
+            let helpers = subc.get_flag("helpers");
+
+            Ok(CallArgs {
+                privileges,
+                command: Command::Render(RenderArguments {
+                    configuration: config,
+                    template,
+                    value_overrides,
+                    shell_trust,
+                    strict,
+                    backend,
+                    helpers,
+                }),
+            })
+        } else {
+            return Err(Box::new(crate::error::Failed::new("unknown subcommand")))
         }
     }
 }
